0d7db8ef688d7c36c119f9c3cbb9a5d3da965b52
Fatima Siddiqui <fatima_siddiqui49@yahoo.com>
2020-07-08 16:51:21 +0530

    Changed link color to navajowhite

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -60,14 +60,14 @@ const BlogPostTemplate = ({ data, pageContext, location }) => {
         >
           <li>
             {previous && (
-              <Link to={previous.fields.slug} rel="prev">
+              <Link style={{color: `navajowhite`}} to={previous.fields.slug} rel="prev">
                 ← {previous.frontmatter.title}
               </Link>
             )}
           </li>
           <li>
             {next && (
-              <Link to={next.fields.slug} rel="next">
+              <Link style={{color: `navajowhite`}} to={next.fields.slug} rel="next">
                 {next.frontmatter.title} →
               </Link>
             )}
